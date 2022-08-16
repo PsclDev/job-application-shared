e1048d00fe1b6d770d206334f56d55c655bd875c
PsclDev <me@pscl.dev>
2022-08-16 17:43:05 +0200

    feat: updated status interface

--- types/application.interface.ts
+++ types/application.interface.ts
@@ -3,6 +3,21 @@ import { FileInterface } from './file.interface';
 import { MeetingInterface } from './meeting.interface';
 import { PersonInterface } from './person.interface';
 
+export enum StatusEnum {
+  PENDING = 'PENDING',
+  SEND = 'SEND',
+  INTERVIEW = 'INTERVIEW',
+  ACCEPTED = 'ACCEPTED',
+  REJECTED = 'REJECTED',
+  NOT_INTERESTED = 'NOT_INTERESTED',
+  NO_ANSWER = 'NO_ANSWER',
+}
+
+export interface Status {
+  status: StatusEnum;
+  date: Date;
+}
+
 export interface ApplicationInterface extends BaseInterface {
   groupId: string;
   name: string;
@@ -10,7 +25,8 @@ export interface ApplicationInterface extends BaseInterface {
   company: string;
   contact: PersonInterface;
   jobUrl: string;
-  status: string;
+  status: Status;
+  history: Status[];
   notes: string;
   meetings: MeetingInterface[];
   files: FileInterface[];
