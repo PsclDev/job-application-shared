dc431febcb68718849d1d6f87c43ebd78b5a9040
PsclDev <me@pscl.dev>
2022-09-14 21:42:01 +0200

    feat: files for group

--- types/group.interface.ts
+++ types/group.interface.ts
@@ -1,9 +1,11 @@
 import { ApplicationInterface } from './application.interface';
 import { BaseInterface } from './base.interface';
+import { FileInterface } from './file.interface';
 
 export interface GroupInterface extends BaseInterface {
   name: string;
   description: string;
   applications: ApplicationInterface[];
+  files: FileInterface[];
   isArchived: boolean;
 }
